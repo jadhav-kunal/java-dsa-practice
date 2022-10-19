236d4ca40a0ab5e238fbfd991453d748cd5ebb94
“personal” <jadhavkunal1999@gmail.com>
2022-10-20 00:50:40 +0530

    first recurring character

---
+++ 2.hashtable/firstRecChar.js
@@ -0,0 +1,37 @@
+// Google question
+// First recurring character
+
+// given an array = [2,5,1,2,3,5,1,2,4]
+// return 2
+
+// given an array = [2,1,1,2,3,5,1,2,4]
+// return 1
+
+// given an array = [2,5,1,3,4]
+// return undefined
+
+function firstRecurringCharacter(arr){
+
+    const map = {}
+    for(let i=0;i<arr.length;i++){
+        for(let j=i+1;j<arr.length;j++){
+         if(arr[i]===arr[j]){
+            return arr[i];
+         }
+        } 
+    }
+    return undefined;
+}
+
+function firstRecurringCharacter2(arr){
+    let map = {};
+    for(let i=0;i<arr.length;i++){
+        if(map[arr[i]]!== undefined ){
+            return arr[i];
+        }else{
+            map[arr[i]] = i;
+        }
+    }
+}
+const result = firstRecurringCharacter2([2,1,1,2,3,5,1,2,4]);
+console.log(result);
