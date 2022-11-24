b02f48d433f1d0a5ec3c5e97df1d7e76d7672896
“personal” <jadhavkunal1999@gmail.com>
2022-11-24 23:32:10 +0530

    implemented recursion

---
+++ 8.recursion/fibonacci.js
@@ -0,0 +1,18 @@
+function fibonacciIterative(n) {
+  let arr = [0, 1];
+  for (let i = 2; i < n + 1; i++) {
+    arr.push(arr[i - 2] + arr[i - 1]);
+  }
+  return arr[n];
+}
+console.log(fibonacciIterative(3));
+
+function fibonacciRecursive(n) {
+  // O(2^n) - exponential time complexity
+  if (n < 2) {
+    return n;
+  }
+  return fibonacciRecursive(n - 1) + fibonacciRecursive(n - 2);
+}
+
+console.log(fibonacciRecursive(6));
