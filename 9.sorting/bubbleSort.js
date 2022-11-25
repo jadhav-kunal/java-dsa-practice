353927e693cf9851d9f8685dfe0e8ff179fa4819
“personal” <jadhavkunal1999@gmail.com>
2022-11-25 11:11:50 +0530

    implemented bubble sort

---
+++ 9.sorting/bubbleSort.js
@@ -0,0 +1,19 @@
+const numbers = [99, 2, 74, 9, 1, 5, 34, 283, 12, 0];
+
+function bubbleSort(array) {
+  const length = array.length;
+  for (let i = 0; i < length; i++) {
+    console.log(i, " --> ", numbers);
+    for (let j = 0; j < length; j++) {
+      if (array[j] > array[j + 1]) {
+        //Swap numbers
+        let temp = array[j];
+        array[j] = array[j + 1];
+        array[j + 1] = temp;
+      }
+    }
+  }
+}
+
+bubbleSort(numbers);
+console.log(numbers);
